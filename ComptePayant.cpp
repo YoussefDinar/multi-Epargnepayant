93d8bb3d64f71afcda69927173d4c79e5ab44ae6
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-16 06:14:18 +1100

    Update ComptePayant.cpp

--- ComptePayant.cpp
+++ ComptePayant.cpp
@@ -9,14 +9,14 @@ ComptePayant::ComptePayant()
 bool ComptePayant::retirerArgent(Devise montant){
 
 this->Compte::retirerArgent(montant);
-this->Compte::retirerArgent(montant*0.5);
+this->Compte::retirerArgent(montant*0.05);
 
 }
 
 void ComptePayant::deposerArgent(Devise montant)
  {
  this->Compte::deposerArgent(montant);
-this->Compte::deposerArgent(montant*0.5);
+this->Compte::deposerArgent(montant*0.05);
 
  }
 
