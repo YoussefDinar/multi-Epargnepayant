e04b522689fb007c11b582cd031b298d5883c31f
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-16 06:33:42 +1100

    Update and rename CpEppy.h to CpEpargne_pay.h

--- CpEpargne_pay.h
+++ CpEpargne_pay.h
@@ -4,14 +4,14 @@
 #include "Devise.h"
 #include<iostream>
 using namespace std;
-class CpEppy : public CompteEpargne, public ComptePayant
+class CpEpargne_pay : public CompteEpargne, public ComptePayant
 {
 
 public:
-	CpEppy(Client* c, Devise*d, double taux);
+	CpEpargne_pay(Client* c, Devise*d, double taux);
 	bool retirerArgent(Devise montant)override;
 	void deposerArgent(Devise montant)override;
-	~CpEppy();
+	~CpEpargne_pay();
 
 
 };
