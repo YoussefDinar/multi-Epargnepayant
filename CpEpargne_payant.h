b7fb12b48c386fddd14840036a15c2e0d6dc22d9
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-16 08:17:57 +1100

    Update and rename CpEpargne_pay.h to CpEpargne_payant.h

--- CpEpargne_payant.h
+++ CpEpargne_payant.h
@@ -3,16 +3,16 @@
 #include "ComptePayant.h"
 #include "Devise.h"
 #include<iostream>
+
+
 using namespace std;
 class CpEpargne_payant : public CompteEpargne, public ComptePayant
 {
-
 public:
-	CpEpargne_payant(Client* c, Devise*d, double taux);
-	bool retirerArgent(Devise montant)override;
-	void deposerArgent(Devise montant)override;
+	CpEpargne_payant(Client* cl, Devise*dev, double tx);
+	bool retirerArgent(Devise mt)override;
+	void deposerArgent(Devise mt)override;
 	~CpEpargne_payant();
-
 
 };
 
