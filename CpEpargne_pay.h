21b915330c7155bff9fbe21cca707ae204cf8257
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-16 08:13:53 +1100

    Update CpEpargne_pay.h

--- CpEpargne_pay.h
+++ CpEpargne_pay.h
@@ -4,14 +4,14 @@
 #include "Devise.h"
 #include<iostream>
 using namespace std;
-class CpEpargne_pay : public CompteEpargne, public ComptePayant
+class CpEpargne_payant : public CompteEpargne, public ComptePayant
 {
 
 public:
-	CpEpargne_pay(Client* c, Devise*d, double taux);
+	CpEpargne_payant(Client* c, Devise*d, double taux);
 	bool retirerArgent(Devise montant)override;
 	void deposerArgent(Devise montant)override;
-	~CpEpargne_pay();
+	~CpEpargne_payant();
 
 
 };
