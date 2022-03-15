59fdf9f3ed613e49dea01d4d0571b28967d8340b
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-16 06:10:19 +1100

    Update CpEppy.cpp

--- CpEppy.cpp
+++ CpEppy.cpp
@@ -5,6 +5,21 @@ CpEppy::CpEppy(Client* c, Devise* d, double taux):
 {
 }
 
+bool ComptePayant::retirerArgent(Devise montant){
+
+this->Compte::retirerArgent(montant);
+this->Compte::retirerArgent(montant*0.05);
+
+}
+
+void ComptePayant::deposerArgent(Devise montant)
+ {
+ this->Compte::deposerArgent(montant);
+this->Compte::deposerArgent(montant*0.05);
+
+ }
+
+
 CpEppy::~CpEppy()
 {
 }
