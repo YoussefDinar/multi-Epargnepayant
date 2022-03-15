72696903449b1ad4cee413b549ad363b610c9aea
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-16 08:15:49 +1100

    Update CpEpargne_payant.cpp

--- CpEpargne_payant.cpp
+++ CpEpargne_payant.cpp
@@ -1,17 +1,17 @@
 #include "CpEpargne_payant.h"
 
-CpEpargne_payant::CpEpargne_payant(Client* c, Devise* d, double taux):
-	CompteEpargne(c,d,taux), Compte(c,d);
+CpEpargne_payant::CpEpargne_payant(Client* cl, Devise* dev, double tx):
+	CompteEpargne(cl,dev,tx), Compte(cl,dev);
 {
 }
 
-bool CpEpargne_payant::retirerArgent(Devise montant){
+bool CpEpargne_payant::retirerArgent(Devise mont){
 
 
 
 }
 
-void CpEpargne_payant::deposerArgent(Devise montant)
+void CpEpargne_payant::deposerArgent(Devise mont)
  {
 
 
