afc12e493976b65a914ede075658073f51a7e4b6
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:22:03 +1100

    Update CompteCourant.h

--- CompteCourant.h
+++ CompteCourant.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "Compte.h"
 #include "Devise.h"
+
+
 class CompteCourant :
     public Compte
 {
-    CompteCourant(Client* c, Devise* s, Devise dec);
+    CompteCourant(Client* c, Devise s, Devise dec);
 
     bool retirerArgent(Devise montant)override;
 
-private: 
+private:
     Devise decouvert;
 
 };
