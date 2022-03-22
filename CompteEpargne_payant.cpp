7a50b44084084db15ff58eb94cc3819d2b160db4
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:25:16 +1100

    Update CompteEpargne_payant.cpp

--- CompteEpargne_payant.cpp
+++ CompteEpargne_payant.cpp
@@ -1,22 +1,31 @@
 #include "CompteEpargne_payant.h"
 
-CompteEpargne_payant::CompteEpargne_payant(Client* cl, Devise* dev, double tx):
-CompteEpargne(cl,dev,tx), Compte(cl,dev);
+CompteEpargne_payant::CompteEpargne_payant(Client* cl, Devise dev, double dc)
+  :CompteEpargne(cl,dev,dc),ComptePayant(),Compte(cl,dev)
 {
 }
 
-bool CompteEpargne_payant::retirerArgent(Devise mont){
-
+bool CompteEpargne_payant::retirerArgent(Devise mont) {
+     
+    this->CompteEpargne::retirerArgent(mont);
 
 }
 
 void CompteEpargne_payant::deposerArgent(Devise mont)
- {
+{
+   
+     
+    this->CompteEpargne::deposerArgent(mont);
+   
 
- }
+}
 
 
+void CompteEpargne_payant::consultersolde() const
+{
+   this->Compte::consultersolde();
+}
 CompteEpargne_payant::~CompteEpargne_payant()
 {
- std::cout<<"destructeur Compte Epargne payant"<<std::endl;
+    std::cout << "destructeur Compte Epargne payant" << std::endl;
 }
