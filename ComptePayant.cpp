3493dd79872e2f7e3af4ef6cbe060b1e9e93a17a
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:12:36 +1100

    Update ComptePayant.cpp

--- ComptePayant.cpp
+++ ComptePayant.cpp
@@ -2,25 +2,28 @@
 #include"Devise.h"
 #include <iostream>
 
-ComptePayant::ComptePayant()            
-    {
-      this->taux = 0.05;
-    }
+ComptePayant::ComptePayant()
+{
+    this->taux = 0.05;
+}
 
-bool ComptePayant::retirerArgent(Devise montant){
-    
-    Devise d = (montant * (this->taux / 100.00));
+bool ComptePayant::retirerArgent(Devise montant) {
+  
+       Devise d = (montant * (this->taux*10));
     this->Compte::retirerArgent(d.operator+(montant));
-
+       
 }
 
 void ComptePayant::deposerArgent(Devise montant)
- {
- Devise d = (montant * (taux / 100));
-    this->Compte::retirerArgent((montant.operator-(d)));
+{
+   Devise d = (montant * (taux *10));
+   this->Compte::deposerArgent((montant.operator+(d)));
 
- }
+}
 
-ComptePayant::~ComptePayant(){
-cout<<"destructeur ComptePayant"<<endl;
+
+
+
+ComptePayant::~ComptePayant() {
+    cout << "destructeur ComptePayant" << endl;
 }
