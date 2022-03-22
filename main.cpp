2c8e233d3ceac96532231aec11537aebd12d74dc
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:54:54 +1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #include"CompteCourant.h"
 #include"ComptePayant.h"
 #include"CompteEpargne_payant.h"
+#include"appbq.h"
 
 
 int main()
@@ -27,6 +28,58 @@ int main()
     C1->deposerArgent(100);
     C1->retirerArgent(100);
      C1->consultersolde();
+   
+   
+   int main()
+{
+   Client* p2 = new Client("p1", "p1");
+    
+    
+  Compte *c4=new Compte(p2,1000);
+    c4->deposerArgent(20);
+  c4->retirerArgent(50);
+ // c4->deposerArgent(10);
+  //  c4->consultersolde();
+    cout<<"********CompteEpargne_payant*********"<<endl;
+    CompteEpargne_payant *C1=new CompteEpargne_payant(p2, 2555, 1);
+    C1->deposerArgent(100);
+    C1->retirerArgent(100);
+    C1->consultersolde();
+   
+   
+   
+   cout<<"************App banque************"<<endl;
+   Appbq tab;
+   
+    tab.Addacc(c4);
+    tab[0].deposerArgent(530);
+    bool x;
+    x = tab[0].retirerArgent(124);
+  tab.afficher();
+   
+   
+   
+ return 0;
+ 
+   
+
+}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    
  return 0;
  
