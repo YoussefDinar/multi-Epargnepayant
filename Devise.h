04645b22a63a84bd72910a94f745a1c65923cd62
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 08:27:30 +1100

    Update Devise.h

--- Devise.h
+++ Devise.h
@@ -10,8 +10,11 @@ public:
 	void afficherDevise();
 	Devise& operator+=(const Devise &d2);
 	Devise& operator-=(const Devise& d2);
-
+        Devise operator-( Devise& d2);
+   
+        Devise operator+( Devise& d2);
 	bool operator>=(Devise& D) const;
+	Devise operator/(double val);
 	Devise operator*(Devise& D);
 	Devise operator*(double D);
 	~Devise();
