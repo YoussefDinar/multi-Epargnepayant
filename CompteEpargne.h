dbaaa149540ba5a07b3902ebdd6c0a9f0e8333cb
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:19:02 +1100

    Update CompteEpargne.h

--- CompteEpargne.h
+++ CompteEpargne.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "Compte.h"
-class CompteEpargne :  public Compte
+
+class CompteEpargne : public virtual Compte
 {
 
-public: 
-	CompteEpargne(); 
-	CompteEpargne(Client*c, Devise*d,double taux); 
-	  void Calcul_TauxInteret(); 
-private: 
+public:
+    CompteEpargne();
+    CompteEpargne(Client* c, Devise d, double taux);
+   void Calcul_TauxInteret();
+    
+private:
 
-	double TauxInteret;
-
-
+    double TauxInteret;
 
 };
