4f646edadf4c345f2ee23f6511426c20f3ed91c0
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:26:38 +1100

    Update Operation.cpp

--- Operation.cpp
+++ Operation.cpp
@@ -1,31 +1,35 @@
 
 #include<iostream>
 #include"Operation.h"
-#include "DateH.h"
+//#include "DateH.h"
 #include "Devise.h"
+
+
 
 int Operation::cpt = 0;
 
-Operation::Operation(Devise md,DateH dt,string lb)
+Operation::Operation(Devise md, string lb)
 {
- this->numop= ++Operation::cpt;
-this->date=dt;
-this->montant=md;
-this->lib=lb;
+    this->numop = ++Operation::cpt;
+    //this->date = dt;
+    this->montant = md;
+    this->lib = lb;
 
 }
 
+
+
 void Operation::display()const {
-    this->date.print();
-    cout << "||";
+    /*this->date.print();
+    cout << "||";*/
     cout << this->numop << "||";
-    this->montant;
+    this->montant.afficherDevise();
     cout << "||";
     cout << this->lib << endl;
 }
 
-Operation::~Operation(){
+Operation::~Operation() {
 
-cout<<"destructeur Operation"<<endl;
+    std::cout << "destructeur Operation" << std::endl;
 
 }
