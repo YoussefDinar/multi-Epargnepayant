66571b3fa0f470e4985a7541a9f4f0f0f5c3417c
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-23 10:22:35 +1100

    Update CompteCourant.cpp

--- CompteCourant.cpp
+++ CompteCourant.cpp
@@ -1,15 +1,15 @@
 #include "CompteCourant.h"
 
-CompteCourant::CompteCourant(Client* c, Devise* s, Devise dec)
-	: Compte(c,s)
+CompteCourant::CompteCourant(Client* c, Devise s, Devise dec)
+    : Compte(c, s)
 {
-	this->decouvert = dec;
+    this->decouvert = dec;
 }
 
 bool CompteCourant::retirerArgent(Devise montant)
 {
-	if(this->checkSolde(&(this->decouvert), montant))
-	return this->Compte::retirerArgent(montant);
+    if (this->Compte::checkSolde(this->decouvert))
+        return this->Compte::retirerArgent(montant);
 
-	return false;
+    return false;
 }
