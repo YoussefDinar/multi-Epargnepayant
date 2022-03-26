dfd0c9d5e0dccf54c2a44ba46dc5a007dfd9177a
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-27 08:42:56 +1100

    Update CompteEpargne_payant.h

--- CompteEpargne_payant.h
+++ CompteEpargne_payant.h
@@ -11,6 +11,7 @@ class CompteEpargne_payant : public CompteEpargne, public ComptePayant
 {
 public:
     CompteEpargne_payant(Client* cl, Devise dev, double dc);
+     CompteEpargne_payant();
    bool retirerArgent(Devise mt)override;
     void deposerArgent(Devise mt)override;
      void consultersolde() const override;
