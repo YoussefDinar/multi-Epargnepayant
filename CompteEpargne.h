baf8eb9d8ff196828513a1354e2cac360322819f
YoussefDinar <94021293+YoussefDinar@users.noreply.github.com>
2022-03-27 08:54:26 +1100

    Update CompteEpargne.h

--- CompteEpargne.h
+++ CompteEpargne.h
@@ -7,6 +7,7 @@ class CompteEpargne : public virtual Compte
 public:
     CompteEpargne();
     CompteEpargne(Client* c, Devise d, double taux);
+    bool retirerArgent(Devise montant);
    void Calcul_TauxInteret();
     
 private:
